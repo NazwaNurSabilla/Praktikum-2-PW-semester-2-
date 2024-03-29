7bdbcf7dd28bb0561123c9e8ef459daea26dde02
NazwaNurSabilla <146807986+NazwaNurSabilla@users.noreply.github.com>
2024-03-29 21:40:37 +0700

    Add files via upload

---
+++ praktikum5/class_lingkaran.php
@@ -0,0 +1,21 @@
+<?php
+class Lingkaran
+{
+    private $jari;
+    const PHI = 3.14;
+
+    function _construct($r)
+    {
+        $this->jari = $r;
+    }
+
+    function getluas()
+    {
+        return self::PHI * $this->jari * $this->jari;
+    }
+
+    function getkeliling()
+    {
+        return 2 * self::PHI * $this->jari;
+    }
+}
